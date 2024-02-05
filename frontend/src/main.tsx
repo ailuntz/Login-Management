121d2d294b3d810f38fa5bdcdb6234b42e860b3a
Misachu10032 <johnzhou.engineer@gmail.com>
2024-02-04 22:28:03 -0700

    remove redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,7 +14,6 @@ import { BrowserRouter } from "react-router-dom"
 
 // 状态管理
 import {Provider} from "react-redux"
-import store from "@/store"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
